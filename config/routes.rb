a19fb9bf5d08b2b90acea756b9e3596262ee2c7c
Jan <janp1krc@gmail.com>
2025-08-06 20:51:46 +0200

    Fixing small linting issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :projects
-  
   # The above gives you stuff below
 
   # get "/projects", to: "projects#index"
